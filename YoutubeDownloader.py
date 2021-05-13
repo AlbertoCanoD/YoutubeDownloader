b546541b808f5542815dd7e15cd52ed658b2314e
Alberto Cano Delgado <canoalboroteo@gmail.com>
2021-05-13 21:10:29 +0200

    Update YoutubeDownloader.py

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -1,25 +1,34 @@
+#Import pytube
 #Importamos pytube
 from pytube import YouTube
 
 #Introducimos el link
+#Enter the link
 link = input("Link del video: ")
 yt = YouTube(link)
 
+#We show all possible download options
 #Mostramos todas las opciones de descarga posible
-#videos = yt.streams.all()
+videos = yt.streams.all()
 #videos = yt.streams.filter(progressive=True)
-videos = yt.streams.filter(only_audio=True)
+#videos = yt.streams.filter(only_audio=True)
+
+#Format of list
 #Formato de la lista
 video =list(enumerate(videos))
 
-#Imprimir los resultados
+#Print video results
+#Imprimir los resultados de videos
 for i in video:
     print(i)
 
 print("Elige el formato deseado")
 dn_option = int(input("Opcion: "))
 dn_video = videos[dn_option]
-#Para descargar
-dn_video.download()#Mostramos todas las opciones de descarga posible
 
-print("Descarga correcta")
+#To download
+#Para descargar
+dn_video.download()
+if dn_video.download():
+    print("Descarga correcta")
+print("Error en la descarga")
