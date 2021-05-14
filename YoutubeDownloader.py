71884cc4df15929f290fbf7df87864795184dc33
Alberto Cano Delgado <canoalboroteo@gmail.com>
2021-05-14 16:39:40 +0200

    starter app

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -9,58 +9,25 @@ yt = YouTube(link)
 
 #We show all possible download options
 #Mostramos todas las opciones de descarga posible
-#videos = yt.streams.all()
-#videos = yt.streams.filter(progressive=True)
-#videos = yt.streams.filter(only_audio=True)
+videos = yt.streams.all()
 
 #Download options with their lists
 #Opciones de descarga con sus listas
+listVid = list(enumerate(videos))
 
-#Video and audio
-videoAudio = yt.streams.filter(progressive=True)
-listVidAud = list(enumerate(videoAudio))
-
-#Only audio
-audio = yt.streams.filter(only_audio=True)
-listAud = list(enumerate(audio, 1))
-
-#Format of list
-#Formato de la lista
-allVA = yt.streams.filter(progressive=True, only_audio=True)
-listAllVA = list(enumerate(allVA))
-
-#List union 
-#listVidAud.extend(listAud)
-
-#for i in listAllVA:
- #   print(i)
 #Print video results
-#Imprimir los resultados de videos
-print("Video y audio")
-for i in listVidAud:
+#Imprimir los resultados
+for i in listVid:
     print(i)
 
-#Print video results
-#Imprimir los resultados de videos
-print("Audio")
-for i in listAud:
-   print(i)
-
-for i in listAllVA:
-   print(i)
-
 print("Elige el formato deseado")
-dn_option = int(input("Opcion: "))
-descarga = listAllVA[dn_option]
+opcion = int(input("Opcion: "))
+descarga = listVid[opcion]
 
 #To download
 #Para descargar
-#dn_video.download()
 try:
     descarga.download()
     print("Descarga correcta")
 except:
     print("Error en la descarga")
-#if dn_video.download():
- #   print("Descarga correcta")
-#print("Error en la descarga")
