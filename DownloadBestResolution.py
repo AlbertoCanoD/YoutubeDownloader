9f52b3173b298a79e33a7c748185d3fd0cb1cfe2
Alberto Cano Delgado <canoalboroteo@gmail.com>
2022-09-21 15:22:33 +0200

    Update DownloadBestResolution.py

--- DownloadBestResolution.py
+++ DownloadBestResolution.py
@@ -2,12 +2,13 @@ from pytube import YouTube
 
 #Ask for the link from user
 link = input("Enter the link of YouTube video you want to download: ")
-yt = YouTube(link)
+yt = YouTube(link, on_progress_callback=progress_func)
 
 #Showing details
 print("Title: ",yt.title)
 print("Number of views: ",yt.views)
 print("Length of video: ",yt.length)
+print("progress_func",)
 
 #Getting the highest resolution possible
 ys = yt.streams.get_highest_resolution()
