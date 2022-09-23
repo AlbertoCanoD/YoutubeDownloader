ef8ee836752102d8557255583a4203485bb1ad84
Alberto Cano Delgado <canoalboroteo@gmail.com>
2022-09-23 13:43:07 +0200

    Exceptions handle

--- AudioDownloader.py
+++ AudioDownloader.py
@@ -1,64 +1,72 @@
-#Import pytube
+import sys
 from pytube import YouTube
 
-#Enter the link
+
+def progress_func(stream, chunk, bytes_remaining):
+    # Size of video
+    size = stream.filesize
+    curr = size - bytes_remaining
+    done = int(50 * curr / size)
+    sys.stdout.write("\r[{}{}] ".format('=' * done, ' ' * (50-done)))
+    sys.stdout.flush()
+
+
+# Ask for the link
 link = input("Link del video: ")
+yt = YouTube(link, on_progress_callback=progress_func)
 
-#yt is a object of YouTube class
-yt = YouTube(link)
-
-#Try to get the title, lenght and author of the video
-#Intentamos obtener el titulo, longitud y autor del video
 try:
-    print("\nTitulo: ", yt.title, "\nDuracion: ", yt.length, " segundos", "\nCanal: ", yt.author)
+    print("\nTitulo: ", yt.title, "\nDuracion: ",
+          yt.length, " segundos", "\nCanal: ", yt.author)
 except:
     print("Intentelo de nuevo, revise el link.")
 
+#Filter only audio streams
 audio = yt.streams.filter(only_audio=True)
-    #List of audio streams
+# List of audio streams
 audioStreams = list(enumerate(audio))
 
 print("\nAudio")
 for a in audioStreams:
-   print(a)
+    print(a)
 
-#Only audio
+# Only audio
 try:
     audio = yt.streams.filter(only_audio=True)
-    #List of audio streams
+    # List of audio streams
     audioStreams = list(enumerate(audio))
 except:
     print("No hay pistas de Audio")
 
-#Print the list of audio
+# Print the list of audio
 print("\nAudio")
 for a in audioStreams:
-   print(a)
+    print(a)
 
-#https://www.youtube.com/watch?v=vOL40lrhE6U
+# https://www.youtube.com/watch?v=vOL40lrhE6U
 
 print("\nElige el formato deseado")
 opcion = int(input("Opcion: "))
 descarga = audioStreams[opcion]
-#print(descarga)
-#descarga.download()
-#listVidAud.extend(listAud)
-#descarga.download()
+# print(descarga)
+# descarga.download()
+# listVidAud.extend(listAud)
+# descarga.download()
 #numero = listVidAud[opcion]
-#numero.download()
+# numero.download()
 
 #st = yt.streams.get_by_itag(listVidAud[opcion])
-#st.download
-#To download
-#Para descargar
-#dn_video.download()
-#try:
- #   descarga.download()
-  #  print("Descarga correcta")
-#except:
- #   print("Error en la descarga")
-#if dn_video.download():
- #   print("Descarga correcta")
+# st.download
+# To download
+# Para descargar
+# dn_video.download()
+# try:
+#   descarga.download()
+#  print("Descarga correcta")
+# except:
+#   print("Error en la descarga")
+# if dn_video.download():
+#   print("Descarga correcta")
 #print("Error en la descarga")
 
-#https://www.youtube.com/watch?v=vOL40lrhE6U
+# https://www.youtube.com/watch?v=vOL40lrhE6U
--- DownloadBestResolution.py
+++ DownloadBestResolution.py
@@ -1,34 +1,50 @@
 import sys
-from pytube import YouTube
+from pytube import YouTube, exceptions
 
 
 def progress_func(stream, chunk, bytes_remaining):
-    curr = stream.filesize - bytes_remaining
-    done = int(50 * curr / stream.filesize)
+    # Size of video
+    size = stream.filesize
+    curr = size - bytes_remaining
+    done = int(50 * curr / size)
     sys.stdout.write("\r[{}{}] ".format('=' * done, ' ' * (50-done)))
     sys.stdout.flush()
 
-def bar(stream, chunk, bytesremaining)
 
-# Ask for the link from user
-link = input("Enter the link of YouTube video you want to download: ")
+# Ask for the link
+#link = input("Enter the link of YouTube video you want to download: ")
+link = "https://www.youtube.com/watch?v=f9NEdDs0skM"  # Private video
 yt = YouTube(link, on_progress_callback=progress_func)
 
 blue = '\033[38;2;173;216;230m'  # Hex ADD8E6
 red = '\033[38;2;255;105;97m'  # Hex FF6961
 reset = '\033[39m'  # Reset terminal color
 
-print(blue + "Title: ", yt.title)
-print("Number of views: ", yt.views)
-print("Length of video: ", yt.length, "s")
+exceptions
 
-# Getting the highest resolution possible
-print(red + "Selecting the highest resolution.")
-ys = yt.streams.get_highest_resolution()
+try:
+    yt = YouTube(link)
+
+except exceptions.VideoPrivate as e:
+    print("Private video", e)
+
+except exceptions.VideoUnavailable as e:
+    print("Video is not found", e)
+
+else:
+    print(blue + "Title: ", yt.title, "\nChannel: ", yt.author,  "\nLength: ",
+          yt.length, "s", "\nViews: ", yt.views)
+    try:
+        # Getting the highest resolution possible
+        print(red + "Selecting the highest resolution.")
+        video = yt.streams.get_highest_resolution()
+    except exceptions.HTMLParseError as e:
+        print(e)
+
 
 # Downloading
 print("Downloading video...")
-if ys.download():
+if video.download():
     print("\nDownload completed!!" + reset)
 else:
     print("\nDownload error." + reset)
