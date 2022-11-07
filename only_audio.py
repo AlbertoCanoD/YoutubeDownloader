408bc0c7debfdae354c0b716e2e1c99699f593f8
Alberto Cano <canoalboroteo@gmail.com>
2022-11-07 13:12:27 +0100

    change functions file an add colors()

--- only_audio.py
+++ only_audio.py
@@ -1,6 +1,6 @@
 import platform
 from pytube import YouTube
-from progress_bar import progress_function
+from useful_functions import progress_function, colors
 
 
 # Ask for the link
@@ -8,13 +8,13 @@ from progress_bar import progress_function
 link = "https://www.youtube.com/watch?v=uVl9C3d7ySY"
 yt = YouTube(link, on_progress_callback=progress_function)
 
-blue = '\033[38;2;173;216;230m'  # Hex ADD8E6
+'''blue = '\033[38;2;173;216;230m'  # Hex ADD8E6
 red = '\033[38;2;255;105;97m'  # Hex FF6961
-reset = '\033[39m'  # Reset terminal color
+reset = '\033[39m'  # Reset terminal color'''
 
-print(blue + "Title: ", yt.title, "\nChannel: ", yt.author,  "\nLength: ",
+print(colors(blue) + "Title: ", yt.title, "\nChannel: ", yt.author,  "\nLength: ",
       yt.length, "s", "\nViews: ", yt.views)
-print(red + "\nSelect the audio stream.\n↳" + reset)
+print(colors(red) + "\nSelect the audio stream.\n↳" + colors(reset))
 
 try:
     # Filter only audio streams
@@ -27,11 +27,16 @@ for audio_stream in audio_streams:  # Print all audio streams
     print(audio_stream)
 
 option = int(input("\nSelect the desired stream: "))
-selected_stream = str(audio_streams[option])
+selected_stream = audio_streams[option]
+print("Selected stream -> " + str(selected_stream))
 
-print("Selected stream -> " + selected_stream)
+audio_filter.itag_index
+selected_stream.index(option)
 
-audio = yt.streams.get_by_itag(selected_stream)
+audio_select = selected_stream.index(option)
+
+audio = yt.streams.get_by_itag(audio_select)
+
 
 '''
 TODO
--- useful_functions.py
+++ useful_functions.py
@@ -17,3 +17,9 @@ def progress_function(stream, chunk, bytes_remaining):
     sys.stdout.write(' ↳ |{bar}| {percent}%\r'.format(
         bar=status, percent=percent))
     sys.stdout.flush()
+
+def colors(color):
+    
+    blue = '\033[38;2;173;216;230m'  # Hex ADD8E6
+    red = '\033[38;2;255;105;97m'  # Hex FF6961
+    reset = '\033[39m'  # Reset terminal color
